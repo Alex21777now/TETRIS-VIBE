6cafb3a7fa9e40d06c74a21d1348998ea3576d27
Alex21777now <banna707@i.ua>
2026-08-15 17:29:48 +0300

    Flags

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import us from './images/us.png';
+import gb from './images/gb.jpg';
 
 /*function App() {
   return (
@@ -26,14 +28,54 @@ function App() {
   return (
     <div className="layout">
       <aside className="sidebar sidebar-left">
-         <div className="logo">TETRIS-VIBE</div>
-         
-         <div className="play-for">
-          <div className="divider"></div>
-          <div className="play-for-title">I play for :</div>
-          <div className="divider"></div>
+  <div className="logo">TETRIS-VIBE</div>
+
+  <div className="play-for">
+    <div className="divider"></div>
+
+       <div className="play-for-title">
+         I play for :
+       </div>
+
+    <div className="divider"></div>
+
+    <div className="game-panel">
+      <div className="game-info">
+        <label className="radio-option">
+          <input type="radio" name="country" />
+          <span>USA</span>
+        </label>
+
+        <div className="scoreboard">
+          <span className="score-label">SCORE</span>
+          <span className="score-value">000000</span>
         </div>
-      </aside>
+      </div>
+
+      <div className="usa-flag" aria-label="American flag">
+       <img src={us} alt="Логотип" style={{ maxWidth: '100%', height: 'auto' }} />
+      </div>
+     <div className="game-panel game-panel-uk">
+  <div className="game-info">
+    <label className="radio-option">
+      <input type="radio" name="country" />
+      <span>UK</span>
+    </label>
+
+    <div className="scoreboard">
+      <span className="score-label">SCORE</span>
+      <span className="score-value">000000</span>
+    </div>
+  </div>
+
+  <div className="uk-flag" aria-label="United Kingdom flag">
+    <img src={gb} alt="Логотип" style={{ maxWidth: '100%', height: 'auto' }} />
+  </div>
+</div>
+    </div>
+
+  </div>
+</aside>
 
       <main className="content">
         Center
